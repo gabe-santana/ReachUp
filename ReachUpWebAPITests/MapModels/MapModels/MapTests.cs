6488392f5a96b288e2c13b8c34466565cc82de80
guilhermedjr <guilhermedjrdjrjan@gmail.com>
2020-09-17 22:47:20 -0300

    Website added

---
+++ ReachUpWebAPITests/MapModels/MapModels/MapTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReachUpWebAPI.MapModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReachUpWebAPI.MapModels.Tests
+{
+    [TestClass()]
+    public class MapTests
+    {
+        [TestMethod()]
+        public void FetchLocalPositionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
