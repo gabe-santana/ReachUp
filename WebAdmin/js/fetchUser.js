5daa5f68432e3b111b37e22a5f1eb096d8910aba
gabriel <gabriel.souza541@etec.sp.gov.br>
2020-09-22 18:25:47 -0300

    Babel added for transpilation of TypeScript  to JavaScript, and minor additions

--- WebAdmin/js/fetchUser.js
+++ WebAdmin/js/fetchUser.js
@@ -1,7 +1,35 @@
-$(function(){
+/// <reference path='PageBase.ts'/>
+
+import { AccountRepository } 
+from '../classes/repositories/AccountRepository';
+
+class fetchUser extends AccountRepository.clsAccountRepository {
+    constructor(){
+    super();
+
+    document.querySelector("#btnLogin").click(function(){
+
+      const email = document.querySelector('#txtEmail').val();
+      const password = document.querySelector('#txtPassword').val();
+      getUser(email, password);
+   })
+
+   async function getUser(email, password)
+   {
+     const nm_user = await this.login(email, password);
+     return nm_user;
+   }
+
+
+}
+
+
+
+
+}
+
+$(() => {
      
-    import { clsAccountRepository } 
-    from '../classes/repositories/AccountRepository.js';
 
     $("#btnLogin").click(function(){
 
@@ -17,4 +45,4 @@ $(function(){
       return nm_user;
     }
 
-})
+})*/
