2a7e212263ffd2e2d2b115c43f5ce1d372b62364
guilhermedjr <guilhermedjrdjrjan@gmail.com>
2020-09-23 19:28:00 -0300

    SQLite connection components added: Todo, DatabaseCreator and DatabaseRepository

---
+++ ReachUpApp/ReachUp/lib/Component/DatabaseCreator.dart
@@ -0,0 +1,56 @@
+Database db;
+
+class DatabaseCreator {
+  static const todoTable = 'todo';
+  static const id = 'id';
+  static const name = 'name';
+  static const info = 'info';
+  static const isDeleted = 'isDeleted';
+
+  static void databaseLog(String functionName, String sql,
+      [List<Map<String, dynamic>> getResult, int postOrUpdateResult]) {
+    print(functionName);
+    print(sql);
+
+    if (getResult != null) {
+      print(getResult);
+    } else if (postOrUpdateResult != null) {
+      print(postOrUpdateResult);
+    }
+  }
+
+  Future<void> createTodoTable(Database db) async {
+    final tableSQL = '''CREATE TABLE $todoTable
+    (
+       $id INTEGER PRIMARY KEY,
+       $name TEXT,
+       $info TEXT,
+       $isDeleted BIT NOT NULL
+    )''';
+
+    await db.execute(tableSQL);
+  }
+
+  Future<String> getDatabasePath(String dbName) async {
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, dbName);
+
+    if (await Directory(dirname(path)).exists()) {
+      await deleteDatabase(path);
+    } else {
+      await Directory(dirname(path)).create(recursive: true);
+    }
+
+    return path;
+  }
+
+  Future<void> initDatabase() async {
+    final path = await getDatabasePath('todo_db');
+    db = await openDatabase(path, version: 1, onCreate: onCreate);
+    print(db);
+  }
+
+  Future<void> onCreate(Database db, int version) async {
+    await createTodoTable(db);
+  }
+}
