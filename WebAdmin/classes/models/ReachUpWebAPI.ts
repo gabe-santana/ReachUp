fb0e25c076a89c5a45f2b8fb2ba52ab14a6f137b
guilhermedjr <guilhermedjrdjrjan@gmail.com>
2020-09-23 17:49:09 -0300

    Bug in model class .ts caused by VS Code fixed

--- WebAdmin/classes/models/ReachUpWebAPI.ts
+++ WebAdmin/classes/models/ReachUpWebAPI.ts
@@ -6,19 +6,6 @@ export module ReachUpWebAPI {
              this._url = 'https://192.168.0.109:8000/api';
          }
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD:WebAdmin/classes/models/models/ReachUpWebAPI.js
-     export class clsReachUpWebAPI {
-        constructor() {
-            this.url = 'https://192.168.0.109:8000/api';
-         }
-
-         async httpGet(str, options = {})
-         {
-            const response = await fetch(this.url + str, {
-=======
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913
          public get url() : string {
            return this._url;
          }
@@ -26,23 +13,12 @@ export module ReachUpWebAPI {
          public async httpGet(str : string, options : Array<string> = [])
          {
             const response = await fetch(this._url + str, {
-<<<<<<< HEAD
-=======
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913:WebAdmin/classes/models/ReachUpWebAPI.ts
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913
+                mode: 'cors',
                 method: 'GET',
                 headers: {
                     'Accept':'application/json',
                     'Content-Type':'application/json',
-<<<<<<< HEAD
                     //...options.headers,
-=======
-<<<<<<< HEAD:WebAdmin/classes/models/models/ReachUpWebAPI.js
-                    ...options.headers,
-=======
-                    //...options.headers,
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913:WebAdmin/classes/models/ReachUpWebAPI.ts
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913
                 },
               })
 
@@ -50,33 +26,16 @@ export module ReachUpWebAPI {
               return json;
          }
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD:WebAdmin/classes/models/models/ReachUpWebAPI.js
-         async httpPost(str, options = {})
-         {
-            const response = await fetch(this.url + str, {
-                method: 'POST',
-                body: options.body,
-                headers: {
-                    'Accept':'application/json',
-                    'Content-Type':'application/json',
-                    ...options.headers,
-=======
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913
          public async httpPost(str : string, options : Array<string> = [])
          {
             const response = await fetch(this._url + str, {
+                mode: 'cors',
                 method: 'POST',
                 //body: options.body,
                 headers: {
                     'Accept':'application/json',
                     'Content-Type':'application/json',
                     //...options.headers,
-<<<<<<< HEAD
-=======
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913:WebAdmin/classes/models/ReachUpWebAPI.ts
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913
                 },
               })
               
@@ -85,34 +44,15 @@ export module ReachUpWebAPI {
 
          }
 
-<<<<<<< HEAD
          public async httpPatch(str : string, options : Array<string> = [])
          {
             const response = await fetch(this._url + str, {
-=======
-<<<<<<< HEAD:WebAdmin/classes/models/models/ReachUpWebAPI.js
-         async httpPatch(str, options = {})
-         {
-            const response = await fetch(this.url + str, {
-=======
-         public async httpPatch(str : string, options : Array<string> = [])
-         {
-            const response = await fetch(this._url + str, {
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913:WebAdmin/classes/models/ReachUpWebAPI.ts
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913
+                mode: 'cors',
                 method: 'PATCH',
                 headers: {
                     'Accept':'application/json',
                     'Content-Type':'application/json',
-<<<<<<< HEAD
                     //...options.headers,
-=======
-<<<<<<< HEAD:WebAdmin/classes/models/models/ReachUpWebAPI.js
-                    ...options.headers,
-=======
-                    //...options.headers,
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913:WebAdmin/classes/models/ReachUpWebAPI.ts
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913
                 },
               })
 
@@ -121,34 +61,15 @@ export module ReachUpWebAPI {
 
          }
 
-<<<<<<< HEAD
          public async httpDelete(str : string, options : Array<string> = [])
          {
             const response = await fetch(this._url + str, {
-=======
-<<<<<<< HEAD:WebAdmin/classes/models/models/ReachUpWebAPI.js
-         async httpDelete(str, options = {})
-         {
-            const response = await fetch(this.url + str, {
-=======
-         public async httpDelete(str : string, options : Array<string> = [])
-         {
-            const response = await fetch(this._url + str, {
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913:WebAdmin/classes/models/ReachUpWebAPI.ts
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913
+                mode: 'cors',
                 method: 'DELETE',
                 headers: {
                     'Accept':'application/json',
                     'Content-Type':'application/json',
-<<<<<<< HEAD
                     //...options.headers,
-=======
-<<<<<<< HEAD:WebAdmin/classes/models/models/ReachUpWebAPI.js
-                    ...options.headers,
-=======
-                    //...options.headers,
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913:WebAdmin/classes/models/ReachUpWebAPI.ts
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913
                 },
               })
                 
@@ -157,14 +78,7 @@ export module ReachUpWebAPI {
 
          }
      }
-<<<<<<< HEAD
 }
-=======
-<<<<<<< HEAD:WebAdmin/classes/models/models/ReachUpWebAPI.js
-=======
-}
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913:WebAdmin/classes/models/ReachUpWebAPI.ts
->>>>>>> ded10ec553451ce4a1052eea58a4b5c5526a7913
 
      /*function clsReachUpWebApi(url)
      {
