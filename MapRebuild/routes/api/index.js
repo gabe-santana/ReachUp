45c28363333d2336ec9c9c0635c9612ad7e0eb9f
gabriel <gabriel.souza541@etec.sp.gov.br>
2020-10-08 19:36:42 -0300

    Root folders updated

--- MapRebuild/routes/api/index.js
+++
@@ -1,6 +0,0 @@
-const mallPlan = require('./map');
-const router = require('express').Router();
-
-router.use('/map', mallPlan);
-
-module.exports = router;
--- MapRebuild/routes/api/map/index.js
+++
@@ -1,7 +0,0 @@
-const routes = require('express').Router();
-
-const get = require('../../../controllers/map/get');
-
-routes.get('/', get);
-
-module.exports = routes;
