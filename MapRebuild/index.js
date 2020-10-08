f6bc92b361588225e4c171f28b1d73349760627e
guilhermedjr <guilhermedjrdjrjan@gmail.com>
2020-10-08 17:12:09 -0300

    MapRebuild updated

--- MapRebuild/index.js
+++ MapRebuild/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 
@@ -13,4 +13,14 @@ const startServer = (port = 999) => {
     });
 };
 
-module.exports = startServer;
+module.exports = startServer;*/
+
+process.title = 'MapRebuild';
+var args = process.argv,
+    port =  args[2] || 7070
+    webServer = require('./server');
+
+webServer.listen(port, function(){
+    console.log('Server started at port ' + port);
+});
+
