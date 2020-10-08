65f4ed206300c37659598b6dfacbc7238a9ee0b6
guilhermedjr <guilhermedjrdjrjan@gmail.com>
2020-10-08 20:45:50 -0300

    MapMaker-API connection in progress

--- map-rebuild/controllers/getMap.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (req, res) => {
-    try{
-      //pegar a planta a partir do MapMaker
-    }
-    catch (err) {
-      res.status(500).json({error: {code: '500', message: err.message}, payload: null});
-    }
-
-};
