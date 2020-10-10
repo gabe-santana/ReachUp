63e63be42b515a168b44fc2a66d80e4d5c6ab118
gabriel <gabriel.souza541@etec.sp.gov.br>
2020-10-10 13:01:36 -0300

    LibraryClasses updated: Fixing repositories bug's

--- LibraryClasses/Classes/User.cs
+++ LibraryClasses/Classes/User.cs
@@ -1,10 +1,9 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-using System.Web;
 using MySql.Data.MySqlClient;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ReachUp
 {
@@ -15,8 +14,8 @@ namespace ReachUp
         public string Name { get; set; }
         [JsonIgnore] public string Password { get; set; }
         public string Role{ get; set; }
-
         public Local AdmLocal { get; set; }
+        public string Token { get; set; }
 
         #endregion
 
