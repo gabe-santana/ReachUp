271301a5c35a83ded99c308784b5034bc4e7e286
guilhermedjr <guilhermedjrdjrjan@gmail.com>
2020-11-03 14:35:59 -0300

    Updating Stored Procedures

--- Database/Scripts/CreateScript_StoredProcedures.sql
+++ Database/Scripts/CreateScript_StoredProcedures.sql
@@ -224,9 +224,11 @@ BEGIN
 END$$
 
 DROP PROCEDURE IF EXISTS pegarFeedback$$
-CREATE PROCEDURE pegarFeedback(pFeedback int)
+CREATE PROCEDURE pegarFeedback(pFeedback int, pTipo int)
 BEGIN
-	SELECT * FROM 	feedback WHERE cd_feedback = pFeedback;
+	SELECT * FROM 	feedback 
+    WHERE cd_feedback = pFeedback
+    AND cd_tipo_feedback = pTipo;
 END$$
 
 DROP PROCEDURE IF EXISTS conectarBeacon$$
@@ -495,23 +497,12 @@ DROP PROCEDURE IF EXISTS acessoFeedbacks$$
 CREATE PROCEDURE acessoFeedbacks(pTipo int, dataInicio date , dataFim date, pGeral bool)
 BEGIN
 	IF pGeral = false THEN
-		IF pTipo = 0 THEN
 				SELECT nm_email_cliente, ds_feedback, qt_estrelas_feedback, dt_feedback FROM feedback 
 				WHERE dt_feedback BETWEEN  dataInicio AND dataFim
 				AND cd_tipo_feedback = pTipo;
-		ELSE
-				SELECT nm_email_cliente, ds_feedback,dt_feedback FROM feedback 
-				WHERE dt_feedback BETWEEN  dataInicio AND dataFim
-				AND cd_tipo_feedback = pTipo;
-		END IF;
 	ELSE
-		IF pTipo = 0 THEN
 				SELECT nm_email_cliente, ds_feedback, qt_estrelas_feedback,dt_feedback FROM feedback 
 				WHERE cd_tipo_feedback = pTipo;
-		ELSE
-				SELECT nm_email_cliente, ds_feedback, dt_feedback FROM feedback 
-				WHERE cd_tipo_feedback = pTipo;
-		END IF;
 	END IF;
 END$$
 
@@ -545,11 +536,8 @@ DROP PROCEDURE IF EXISTS pegarLocal$$
 CREATE PROCEDURE pegarLocal(pID INT)
 BEGIN
 	SELECT 
-    l.cd_tipo_local,
-    l.cd_local, tl.nm_tipo_local, l.nm_local, l.cd_andar, 
-	l.hr_abertura, l.hr_fechamento,
-	substring_index(group_concat(DISTINCT c.nm_categoria SEPARATOR ','), ',', 3) as  categorias,
-	b.cd_uuid_beacon AS  Beacons FROM `local` AS l 
+    tl.nm_tipo_local, l.nm_local, l.cd_andar, 
+	group_concat(b.cd_uuid_beacon) AS  Beacons FROM `local` AS l 
 	INNER JOIN tipo_local AS tl
 	ON l.cd_tipo_local = tl.cd_tipo_local
 	INNER JOIN beacon AS b
