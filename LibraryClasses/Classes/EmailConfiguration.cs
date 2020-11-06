746cbe31ec8e4e57f53430e813d13ec9d0240eaa
guilhermedjr <guilhermedjrdjrjan@gmail.com>
2020-11-06 16:21:07 -0300

    WORKING ON EMAIL GITHUB FDP

---
+++ LibraryClasses/Classes/EmailConfiguration.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace ReachUp
+{
+    public class EmailConfiguration
+    {
+       public string From {get; set;}
+       public string SmtpServer {get; set;}
+       public int Port {get; set;}
+       public string UserName {get; set;}
+       public string Password {get; set;}
+
+       public string UserEmail {get; set;}
+       public string EmailName {get; set;}
+       public string EmailContent {get; set;}
+
+       public EmailConfiguration(string userEmail, string emailName, string emailContent)
+       {
+          this.UserEmail = userEmail;
+          this.EmailName = emailName;
+          this.EmailContent = emailContent;
+       }
+       
+    }
+}
