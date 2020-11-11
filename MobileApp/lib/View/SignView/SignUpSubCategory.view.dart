dc3bd588e73750259b3879b17c99d5dda06b02db
gabriel <gabriel.souza541@etec.sp.gov.br>
2020-11-11 19:14:56 -0300

    InitialScreens added | Endpoint access data updated to FutureBuilders snapshot view's

---
+++ MobileApp/lib/View/SignView/SignUpSubCategory.view.dart
@@ -0,0 +1,145 @@
+import 'package:ReachUp/Component/Dialog/CustomDialog.component.dart';
+import 'package:ReachUp/Controller/SubCategory.controller.dart';
+import 'package:ReachUp/Model/Category.model.dart';
+import 'package:ReachUp/Model/Subcategory.model.dart';
+import 'package:ReachUp/View/HomeView/Home.view.dart';
+import 'package:ReachUp/globals.dart';
+import 'package:async/async.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'SubCategoryCard.view.dart';
+
+class SignUpSubCategory extends StatefulWidget {
+  SignUpSubCategory();
+
+  _SignUpSubCategoryState createState() => _SignUpSubCategoryState();
+}
+
+class _SignUpSubCategoryState extends State<SignUpSubCategory> {
+  SubCategoryController subCategoryController = new SubCategoryController();
+  int categoryIndex = 0;
+
+  @override
+  initState() {
+    super.initState();
+  }
+
+  Widget buildListView(List<SubCategory> subCategoriesByCat) {
+    print("length");
+    print(subCategoriesByCat.length);
+
+    return ListView.builder(
+      itemCount: subCategoriesByCat.length,
+      itemBuilder: (context, index) =>
+          SubCategoryCard(subCategoriesByCat[index]),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    void showSnackBarMessage(ctx, String message,
+        [MaterialColor color = Colors.red]) {
+      Scaffold.of(ctx).showSnackBar(SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      ));
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+              " Para ${Globals.categoriesChecked[categoryIndex].categoryName}"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) => CustomDialog(
+                            icon: Icons.info_outline,
+                            title: "Info",
+                            description:
+                                "Selecione as especificações de suas preferências",
+                            buttonOK: RaisedButton(
+                              color: Theme.of(context).colorScheme.primary,
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text(
+                                "OK",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ));
+                },
+                icon: Icon(Icons.info_outline, color: Colors.white))
+          ],
+        ),
+        resizeToAvoidBottomInset: true,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: bodyBuilder(),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: RaisedButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Próxima etapa ',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      FaIcon(FontAwesomeIcons.arrowRight,
+                          color: Colors.white, size: 16)
+                    ],
+                  ),
+                  onPressed: () {
+                    print("index cat");
+                    print( Globals.categoriesChecked.length -      categoryIndex);
+                    if (Globals.subCategoriesChecked.isNotEmpty) {
+                        if(categoryIndex < Globals.categoriesChecked.length - 1){
+                          setState(() {
+                            categoryIndex++;
+                          });
+                        }
+                        else{
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+                        }
+                    } else
+                      showSnackBarMessage(
+                          context, "Escolha pelo menos um item!");
+                  },
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  Widget bodyBuilder() {
+    return FutureBuilder(
+      future: subCategoryController
+          .getByCategory(Globals.categoriesChecked[categoryIndex].categoryId),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          debugPrint('Step 3, build widget: ${snapshot.data}');
+          // Build the widget with data.
+          return buildListView(snapshot.data);
+        } else {
+          // We can show the loading view until the data comes back.
+          debugPrint('Step 1, build loading widget');
+          return Center(
+              child: CircularProgressIndicator(
+                  valueColor: new AlwaysStoppedAnimation<Color>(
+                      Theme.of(context).colorScheme.primary)));
+        }
+      },
+    );
+  }
+}
--- MobileApp/lib/globals.dart
+++ MobileApp/lib/globals.dart
@@ -1,7 +1,14 @@
+import 'package:ReachUp/Model/Category.model.dart';
+
+import 'Model/Subcategory.model.dart';
 import 'Model/User.model.dart';
 
 class Globals{
   static User user;
   static bool isLoggedIn = false;
   static String urlAPI;
+
+
+  static List<Category> categoriesChecked = new List<Category>();
+  static List<SubCategory> subCategoriesChecked = new List<SubCategory>();
 }
