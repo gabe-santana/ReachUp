920a06e14af31d32723703de0a4ec41dcc30a5f0
guilhermedjr <guilhermedjrdjrjan@gmail.com>
2020-11-11 18:04:25 -0300

    Don't even know what this is. Some people need to know how maintain the project organized.

---
+++ Trash/reachupadmin-1/reachupadmin/reachupadmin/App_Start/BundleConfig.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Optimization
+
+Public Class BundleConfig
+    ' Para obter mais informações sobre o Agrupamento, visite https://go.microsoft.com/fwlink/?LinkID=303951
+    Public Shared Sub RegisterBundles(ByVal bundles As BundleCollection)
+        bundles.Add(New ScriptBundle("~/bundles/WebFormsJs").Include(
+                        "~/Scripts/WebForms/WebForms.js",
+                        "~/Scripts/WebForms/WebUIValidation.js",
+                        "~/Scripts/WebForms/MenuStandards.js",
+                        "~/Scripts/WebForms/Focus.js",
+                        "~/Scripts/WebForms/GridView.js",
+                        "~/Scripts/WebForms/DetailsView.js",
+                        "~/Scripts/WebForms/TreeView.js",
+                        "~/Scripts/WebForms/WebParts.js"))
+
+        ' A ordem é muito importante para que esses arquivos funcionem; eles possuem dependências explícitas
+        bundles.Add(New ScriptBundle("~/bundles/MsAjaxJs").Include(
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjax.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js"))
+
+        ' Use a versão de Desenvolvimento do Modernizr para se desenvolver e aprender com ele. Em seguida, quando estiver
+        ' pronto para a produção, utilize a ferramenta de build em https://modernizr.com para escolher somente os testes que precisa
+        bundles.Add(New ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"))
+    End Sub
+End Class
---
+++ Trash/reachupadmin-1/reachupadmin/reachupadmin/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As Informações Gerais sobre um assembly são controladas por meio do 
+' conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+' associadas a um assembly.
+
+' Verificar os valores dos atributos do conjunto
+<Assembly: AssemblyTitle("reachupadmin")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("reachupadmin")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+<Assembly: Guid("10154ddc-1b6e-44f7-ad9e-13ff896f5069")>
+
+' As informações de versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' É possível especificar todos os valores ou utilizar como padrão os Números da Versão e de Revisão 
+' usando o '*' como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
