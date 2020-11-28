4463d7f0f7e916eb2d5835efcc32da85e8cad9c1
Guilherme Djrdjrjan Santos <44452846+guilhermedjr@users.noreply.github.com>
2020-11-27 23:51:26 -0300

    Shopkeeper addition working

--- WebAdmin/src/Controllers/UserController.js
+++ WebAdmin/src/Controllers/UserController.js
@@ -3,4 +3,22 @@ class UserController {
     constructor(){
         this.api = new ApiConnection();
     }
+
+    async add(shopkeeper, localId)
+    {
+        return await
+         this.api.httpAnonymousPost(
+             'User/',
+             {
+                 name: shopkeeper.Name,
+                 email: shopkeeper.Email,
+                 password: shopkeeper.Password,
+                 role: 'loj',
+                 admLocal: {
+                     idLocal: localId
+                 }
+             }
+         )
+
+    }
 }
