c20b9d673eff7f351392361d881594000eefcfc8
Guilherme Djrdjrjan Santos <44452846+guilhermedjr@users.noreply.github.com>
2020-11-29 01:23:47 -0300

    Working on feedbacks ordination

--- WebAdmin/src/Scripts/feedbackType.js
+++ WebAdmin/src/Scripts/feedbackType.js
@@ -3,14 +3,15 @@ $( async () => {
     
     const clsFeedbackType = new FeedbackTypeController();
     const feedbackTypes = await clsFeedbackType.getAll();
-    console.log(feedbackTypes);
- 
+
     const cmbTypes = $("#cmbFeedbackType");
  
      feedbackTypes.forEach(feedbackType => {
       const option = document.createElement('option');
+      option.id = feedbackType.id;
       option.value = feedbackType.id;
       option.innerText = feedbackType.name;
       cmbTypes.append(option);
     })
+
  })
--- WebAdmin/src/Scripts/feedbacks.js
+++ WebAdmin/src/Scripts/feedbacks.js
@@ -1,8 +1,9 @@
-$( () => {
+$( async () => {
 
     const clsFeedback = new FeedbackController();
+    const clsFeedbackType = new FeedbackTypeController();
 
-    getByType(0);
+    getAll();
 
     function turnFeedbacksOff()
     {
@@ -10,9 +11,8 @@ $( () => {
         feedbacksSection.empty();
     }
 
-    function showFeedbacks(feedbacks)
+    function showAllFeedbacks(feedbacks)
     {
-        turnFeedbacksOff();
         const feedbacksSection = $("#feedbacks");
 
         feedbacks.forEach(feedback => {
@@ -34,15 +34,20 @@ $( () => {
 
             div.append(div2);
 
-            const pText = document.createElement('p');
-            pText.innerText = feedback.description;
-            pText.classList.add('mb-1');
-            div.append(pText);
-
             const pType = document.createElement('p');
             pType.innerText = feedback.typeName;
             pType.classList.add('mb-1');
             div.append(pType);
+
+            const pDate = document.createElement('p');
+            pDate.innerText = feedback.feedbackDate;
+            pDate.classList.add('mb-1');
+            div.append(pDate);
+
+            const pText = document.createElement('p');
+            pText.innerText = feedback.description;
+            pText.classList.add('mb-1');
+            div.append(pText);
 
             const span1 = document.createElement('span');
             span1.classList.add("fa"); 
@@ -99,6 +104,189 @@ $( () => {
         })
     }
 
+    function reorderFeedbacks(feedbacksHTML)
+    {
+        const feedbacksSection = $("#feedbacks");
+        feedbacksSection.remove();
+
+        console.log(feedbacksHTML);
+
+        /*feedbacksSection.forEach(section => {
+
+            for (let i = 0; i < section.length; i++)
+        {
+           console.log(f[i]);
+           feedbacksSection.append(section[i]);
+        }
+
+        })*/
+    }
+
+    function showFeedbacks(feedbacks)
+    {
+        turnFeedbacksOff();
+        const feedbacksSection = $("#feedbacks");
+
+        feedbacks.forEach(feedback => {
+
+            const div = document.createElement('div');
+            div.classList.add('list-group-item');
+            div.classList.add('list-group-item-action');
+
+            const div2 = document.createElement('div');
+            div2.classList.add('d-flex');
+            div2.classList.add('w-100');
+            div2.classList.add('justify-content-between');
+
+            const h5 = document.createElement('h5');
+
+            h5.innerText = feedback.feedbackUser.name;
+            h5.classList.add('mb-1');
+            div2.append(h5);
+
+            div.append(div2);
+
+            const pType = document.createElement('p');
+            pType.innerText = feedback.typeName;
+            pType.classList.add('mb-1');
+            div.append(pType);
+
+            const pDate = document.createElement('p');
+            pDate.innerText = feedback.feedbackDate;
+            pDate.classList.add('mb-1');
+            div.append(pDate);
+
+            const pText = document.createElement('p');
+            pText.innerText = feedback.description;
+            pText.classList.add('mb-1');
+            div.append(pText);
+
+            const span1 = document.createElement('span');
+            span1.classList.add("fa"); 
+            span1.classList.add("fa-star");
+
+            const span2 = document.createElement('span');
+            span2.classList.add("fa"); 
+            span2.classList.add("fa-star");
+
+            const span3 = document.createElement('span');
+            span3.classList.add("fa"); 
+            span3.classList.add("fa-star");
+
+            const span4 = document.createElement('span');
+            span4.classList.add("fa"); 
+            span4.classList.add("fa-star");
+
+            const span5 = document.createElement('span');
+            span5.classList.add("fa"); 
+            span5.classList.add("fa-star");
+
+            const starsAmount = feedback.stars;
+
+            for (var i = 0; i < starsAmount; i++)
+            {
+                switch (i) 
+                {
+                    case 0:
+                        span1.classList.add("checked");
+                    break;
+                    case 1:
+                        span2.classList.add("checked");
+                    break;
+                    case 2:
+                        span3.classList.add("checked");
+                    break;
+                    case 3:
+                        span4.classList.add("checked");
+                    break;
+                    case 4:
+                        span5.classList.add("checked");
+                    break;
+                }
+            }
+
+            div.append(span1);
+            div.append(span2);
+            div.append(span3);
+            div.append(span4);
+            div.append(span5);
+
+            feedbacksSection.append(div);
+
+        })
+    }
+
+    function sortAscending()
+    {
+        const feedbacksSection = $("#feedbacks");
+        const feedbacks = Array.from(feedbacksSection);
+        var dates = [];
+
+        feedbacks.forEach(feedback => {
+            
+            for (let i = 0; i < feedback.children.length; i++)
+            {
+               dates.push(feedback.children[i]);
+            }
+        })
+
+        dates.sort(function(a, b) {
+            if (a.innerHTML.substr(a.innerHTML.split("<p class='mb-1'>", 2) + 6, 4) 
+             > b.innerHTML.substr(b.innerHTML.split("<p class='mb-1'>", 2) + 6, 4))
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        })
+
+        dates.sort(function(a, b) {
+            if (a.innerHTML.substr(a.innerHTML.split("<p class='mb-1'>", 2) + 3, 2) 
+            > b.innerHTML.substr(b.innerHTML.split("<p class='mb-1'>", 2) + 3, 2))
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        })
+
+        dates.sort(function(a, b) {
+            if (a.innerHTML.substr(a.innerHTML.split("<p class='mb-1'>", 2), 2)
+            >  b.innerHTML.substr(b.innerHTML.split("<p class='mb-1'>", 2), 2))
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        })
+
+        reorderFeedbacks(feedbacks);
+    }
+
+    function sortDescending(){}
+
+    async function getAll()
+    {
+        const feedbackTypes = await clsFeedbackType.getAll();
+ 
+        feedbackTypes.forEach(feedbackType => {
+           getAllByType(feedbackType.id);
+        })
+    }
+
+    async function getAllByType(type)
+    {
+        const feedbacks = await clsFeedback.getAll(type);
+        console.log(feedbacks);
+        showAllFeedbacks(feedbacks);
+    }
+
     async function getByType(type)
     {
         const feedbacks = await clsFeedback.getAll(type);
@@ -151,8 +339,16 @@ $( () => {
         }
      })
 
-     $("#btnSearchByUser").click( async () => {
+     $("#txtClientEmail").change( async () => {
          const email = $("#txtClientEmail").val();
          await getByUser(email);
      })
+
+     $("#btnNewestFirst").click(() => {
+        sortAscending();
+     })
+
+     $("#btnOldestFirst").click(() => {
+        sortDescending();
+    })
 })
